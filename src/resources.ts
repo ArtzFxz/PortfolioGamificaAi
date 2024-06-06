33560bb786cf2dca04a9c3a2eadf56a4d46375d7
Artz.Fxz <felipepolegatofr@gmail.com>
2024-06-06 17:16:52 -0300

    feat: Gamification

--- src/resources.ts
+++ src/resources.ts
@@ -2,13 +2,15 @@ import { ImageSource, Loader } from "excalibur";
 import sword from "./images/sword.png";
 import logo from "./images/logo.png";
 import logover from "./images/logover.png";
+import gamifi from "./images/gamification.png"
 
 
 
 export const Resources = {
   Sword: new ImageSource(sword),
   Logo: new ImageSource(logo),
-  Logover: new ImageSource(logover)
+  Logover: new ImageSource(logover),
+  Gamific: new ImageSource(gamifi)
 } as const;
 
 export const loader = new Loader();
--- src/scenes/gamificationScene.ts
+++ src/scenes/gamificationScene.ts
@@ -1,7 +1,47 @@
-import { Color, Engine, Scene } from "excalibur";
+import { Actor, Color, Engine, Scene, vec } from "excalibur";
+import { Resources } from "../resources";
 
 export class gamificationScene extends Scene {
+    elementText?: HTMLElement
+
     onInitialize(engine: Engine<any>): void {
-        this.backgroundColor = Color.Chartreuse
+        this.backgroundColor = Color.fromHex("#403f4c")
+
+        // Configurar Actor de gamificação
+        let actorGamificIMG = new Actor({
+            pos: vec(350, 400)
+        })
+
+        // Utilizar imagem do logo
+        let imagemGamific = Resources.Gamific.toSprite()
+
+        // Aplicar zoom na imagem - 40% de x, e 40% de y
+        imagemGamific.scale = vec(1.4, 1.4)
+
+        // Configurar o actor para usar a imagem
+        actorGamificIMG.graphics.add(imagemGamific)
+
+        // Adicionando Actor Logo na tela
+        this.add(actorGamificIMG)
+
+
+        // Criar elemento com a descrição da empresa
+        this.elementText = document.createElement("div") as HTMLElement 
+
+
+        // Definir opcadidade do elemento para 1 = visivel
+        this.elementText.style.opacity = "1"
+
+        // Inserir elementoTexto no container-game
+        let containerGame = document.querySelector(".container-game") as HTMLElement
+        containerGame.appendChild(this.elementText)
+
+        // Adicionar classe na div criada (elementoTexto)
+        this.elementText.classList.add("gamificacao")
+
+        // Adicionar titulo e paragrafo dentro do conteudo da div
+        this.elementText.innerHTML = `<h2>O que é Gamificação</h2>
+        <p>Gamificação é a aplicação de elementos típicos de jogos em contextos não lúdicos, com o objetivo de engajar e motivar indivíduos a atingir determinados objetivos. Esta abordagem se utiliza de componentes como pontuação, níveis, recompensas, desafios, e feedback imediato, visando promover comportamentos desejados e aumentar a participação e o comprometimento dos participantes.</p>`
+
     }
 }
