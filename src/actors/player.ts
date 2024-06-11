f007a0c9721d52e3e3f8c075bfaab8d1582bcda3
Artz.Fxz <felipepolegatofr@gmail.com>
2024-06-11 17:18:24 -0300

    feat: Player

---
+++ src/actors/player.ts
@@ -0,0 +1,74 @@
+import { Actor, Color, Engine, Keys, vec } from "excalibur";
+
+export class Player extends Actor {
+    // Propriedade 
+    private velocidade: number = 180
+
+    // Configuração do Player
+    constructor() {
+        super({
+            pos: vec(600, 500),
+            width: 32,
+            height: 32,
+            name: "Jogador",
+            color: Color.Red
+        })
+    }
+
+    onInitialize(engine: Engine<any>): void {
+        // Configurar player para monitorar evento "hold" -> segurar tecla
+        engine.input.keyboard.on("hold", (event) => {
+            // Detectar qual tecla esta pressionada
+            switch (event.key) {
+                case Keys.Left:
+                case Keys.A:
+                    // Mover para esquerda
+                    // Define a velocidade x para negativa, que significa movimentar o player para a esquerda
+                    this.vel.x = -this.velocidade
+                    break;
+
+                case Keys.Right:
+                case Keys.D:
+                    // Mover para a direita
+                    // Define a velocidade x para positiva, que significa movimentar o player para a direita
+                    this.vel.x = this.velocidade
+                    break;
+
+                case Keys.Up:
+                case Keys.W:
+                    // Mover para cima
+                    // Define a velocidade y para negativa, que significa movimentar o player para cima
+                    this.vel.y = -this.velocidade
+                	break;
+
+                case Keys.Down:
+                case Keys.S:
+                    // Mover para baixo
+                    // Define a velocidade y para positiva, que significa movimentar o player para baixo
+                    this.vel.y = this.velocidade
+                    break;
+
+                default:
+                    // Zera a velocidade do player, PARA a movimentacao
+                    this.vel.x = 0
+                    this.vel.y = 0
+                    break;
+            }
+        })
+
+        // Configura o player para monitorar evento "release" -> soltar
+        engine.input.keyboard.on("release", (event) => {
+            // Fazer o player parar ao soltar as teclas de mov.
+            // Parar mov lateral.
+            if(
+                event.key == Keys.A ||
+                event.key == Keys.Left ||
+                event.key == Keys.D ||
+                event.key == Keys.Right
+            ) {
+
+            }
+        })
+    }
+
+}
